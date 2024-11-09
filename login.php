6d2f80f77969c86effc3e19a636e31e23964ba9a
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2024-11-09 23:59:11 +0000

    working on db_connection

--- login.php
+++ login.php
@@ -5,6 +5,26 @@
     // start session
     session_start();
 
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $username = trim($_POST('uname'));
+        $password = trim($_POST(('psw')));
+
+        if(empty($username) || empty($password)){
+            echo "Please fill in all fields";
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
     
 
 
