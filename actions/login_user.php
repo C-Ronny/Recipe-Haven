ef3ffb5c042284222e713f16fdccbf2d82efe120
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2024-11-16 23:42:38 +0000

    route to dashboard after superuser login

--- actions/login_user.php
+++ actions/login_user.php
@@ -43,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             // Redirect to appropriate page
             if ($userrole == 1) {
-                header('Location: dashboard.php');
+                header('Location: ../view/dashboard.php');
             } else {
                 header('Location: ../view/recipefeed.php');
             }
